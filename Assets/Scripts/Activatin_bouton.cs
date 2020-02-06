dea0972b5911664b0d211930e2c56b821bed6cdb
Dubois Thomas <thomas.dubois@isen.yncrea.fr>
2020-02-06 16:43:39 +0100

    Phase 4
    UI

---
+++ Assets/Scripts/Activatin_bouton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement ; 
+
+namespace DefaultNamespace{
+public class Activatin_bouton : MonoBehaviour
+{
+	[SerializeField] private Image backgroundImage = default;
+	private bool isToggled = false;
+
+	public void OnClick(){
+		//isToggled =! isToggled;
+		//backgroundImage.color = isToggled ? Color.blue : Color.red; // remplace un if 
+		SceneManager.LoadScene("SampleScene");
+	}
+}
+}
